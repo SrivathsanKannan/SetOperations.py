bb5c0dde8adfea0bff09aac73fdfa57d2989e6ad
SrivathsanKannan <67915515+SrivathsanKannan@users.noreply.github.com>
2021-11-10 18:28:15 +0400

    Created SetOperations.py

---
+++ SetOperations.py
@@ -0,0 +1,16 @@
+# Program to perform illustrate various set operations
+
+E = {1, 2, 3, 4, 5, 6};
+N = {1, 3, 5, 7, 9, 11};
+ 
+# Union
+print("Union of E and N is",E | N)
+ 
+# Intersection
+print("Intersection of E and N is",E & N)
+ 
+# Difference
+print("Difference of E and N is",E - N)
+ 
+# Symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
